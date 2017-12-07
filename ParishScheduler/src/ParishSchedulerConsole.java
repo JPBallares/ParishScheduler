54c90bc208e6017913cc557bb71af872864433f1
Dominic <2166655@slu.edu.ph>
2017-12-07 14:34:07 +0800

    a

--- ParishScheduler/src/ParishSchedulerConsole.java
+++ ParishScheduler/src/ParishSchedulerConsole.java
@@ -1,5 +1,13 @@
-
+import java.util.*;
 public class ParishSchedulerConsole {
+	private Scanner kbd = new Scanner(System.in);
+	
+	public void enterSchedule() {
+		System.out.print("Enter start time : ");
+		int startTime = kbd.nextInt();
+		System.out.print("Enter Priest name : ");
+		String priestName = kbd.nextLine();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
