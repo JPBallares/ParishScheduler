6bbc01d0f0de48b53454b1ac2a304819c229402a
Dominic <2166655@slu.edu.ph>
2017-12-07 15:17:42 +0800

    Update checktime

--- ParishScheduler/src/ParishSchedulerConsole.java
+++ ParishScheduler/src/ParishSchedulerConsole.java
@@ -7,25 +7,26 @@ public class ParishSchedulerConsole {
 		String[] t = time.split(":");
 		String[] ap = time.split(" ");
 		
-		if ((t[0])) {
-			
+		if ( ((Integer.parseInt(t[0]) >= 7) && (ap[1].equals("AM"))) || ((Integer.parseInt(t[0]) <= 6) && (ap[1].equals("PM"))) ) {
+			flag = true;
 		}
 		return flag;
 	}
 	
 	public static void enterSchedule() {
+		String startTime;
 		do {
 			System.out.println("Example: 7:00 AM");
 			System.out.print("Enter start time : ");
-			String startTime = kbd.nextLine();
-		} while ();
+			startTime = kbd.nextLine();
+		} while (checkTime(startTime.toUpperCase()) == false);
 		
 		System.out.print("Enter Priest name : ");
 		String priestName = kbd.nextLine();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		enterSchedule();
 	}
 
 }
