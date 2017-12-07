a31deb12bbdb88d49bdc62b897e052736f0ea4c2
Dominic <2166655@slu.edu.ph>
2017-12-07 14:56:38 +0800

    Add method checkTime

--- ParishScheduler/src/ParishSchedulerConsole.java
+++ ParishScheduler/src/ParishSchedulerConsole.java
@@ -1,17 +1,31 @@
 import java.util.*;
 public class ParishSchedulerConsole {
-	private Scanner kbd = new Scanner(System.in);
+	private static Scanner kbd = new Scanner(System.in);
 	
-	public void enterSchedule() {
-		System.out.print("Enter start time : ");
-		int startTime = kbd.nextInt();
+	public static boolean checkTime(String time) {
+		boolean flag = false;
+		String[] t = time.split(":");
+		String[] ap = time.split(" ");
+		
+		if ((t[0])) {
+			
+		}
+		return flag;
+	}
+	
+	public static void enterSchedule() {
+		do {
+			System.out.println("Example: 7:00 AM");
+			System.out.print("Enter start time : ");
+			String startTime = kbd.nextLine();
+		} while ();
+		
 		System.out.print("Enter Priest name : ");
 		String priestName = kbd.nextLine();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
