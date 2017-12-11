81b254e94d6e73552dc3395de2821158d176185c
Don <pocaandominic@gmail.com>
2017-12-11 22:44:21 +0800

    Added method run and showMenu.

--- ParishScheduler/src/ParishSchedulerConsole.java
+++ ParishScheduler/src/ParishSchedulerConsole.java
@@ -2,17 +2,63 @@ import java.util.*;
 public class ParishSchedulerConsole {
 	private static Scanner kbd = new Scanner(System.in);
 	
+	/**
+	 * 
+	 * @param time
+	 * @return
+	 */
 	public static boolean checkTime(String time) {
 		boolean flag = false;
 		String[] t = time.split(":");
 		String[] ap = time.split(" ");
 		
-		if ( ((Integer.parseInt(t[0]) >= 7) && (ap[1].equals("AM"))) || ((Integer.parseInt(t[0]) <= 6) && (ap[1].equals("PM"))) ) {
+		if ( ((Integer.parseInt(t[0]) >= 7) && (ap[1].toUpperCase().equals("AM"))) || ((Integer.parseInt(t[0]) <= 6) 
+				&& (ap[1].toUpperCase().equals("PM"))) ) {
 			flag = true;
 		}
 		return flag;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	public static int showParishMenu() {
+		int choice;
+		System.out.println("========================================================");
+		System.out.printf("%1s%29s%27s", "=", "MENU", "=\n");
+		System.out.println("========================================================");
+		System.out.printf("%1s%5s%34s", "= ", "1. View mass Schedule", "=\n");
+		System.out.printf("%1s%5s%25s", "= ", "2. Schedule intention for mass", "=\n");
+		System.out.printf("%1s%5s%32s", "= ", "3. View Priest Schedule", "=\n");
+		System.out.printf("%1s%5s%47s", "= ", "4. Exit ", "=\n");
+		System.out.print("  Enter choice: ");
+		choice = kbd.nextInt();
+		return choice;
+	}
+	
+	/**
+	 * 
+	 */
+	public static void run() {
+		int choice;
+		choice = showParishMenu();
+		System.out.print(choice);
+		switch(choice){
+			case 1:
+
+			case 2:
+
+			case 3:
+
+			case 4:
+				System.exit(0);
+		}
+	}
+	
+	/**
+	 * 
+	 */
 	public static void enterSchedule() {
 		String startTime;
 		do {
@@ -24,9 +70,19 @@ public class ParishSchedulerConsole {
 		System.out.print("Enter Priest name : ");
 		String priestName = kbd.nextLine();
 	}
-
+	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		enterSchedule();
+		ParishSchedulerConsole ps;
+		try{
+			ps = new ParishSchedulerConsole();
+			ps.run();
+		}catch(Exception x){
+			x.printStackTrace();
+		}
+		System.exit(0);
 	}
-
 }
